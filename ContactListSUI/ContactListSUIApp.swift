07e0a2b4845bb887adb8dc9ba1ae1c9681261bc3
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-06-23 06:42:26 +0300

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by Kasharin Mikhail on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
